cb8fe8acad45d6a04014f4960fd4c92889188045
olibrahim <olayodeibrahim123@gmail.com>
2022-09-08 11:43:21 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -  print exactly "Programming is like building a multilingual puzzle.
- *
- * Return: Always 0 (Sucess)
- */
+* main -  print exactly "Programming is like building a multilingual puzzle.
+*
+* Return: Always 0 (Sucess)
+*/
 
- int main(void)
- {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- }
+}
