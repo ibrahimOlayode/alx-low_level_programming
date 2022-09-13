0c81a74fcbb3e183e3baef9b38a24e2625ecc4f4
olibrahim <olayodeibrahim123@gmail.com>
2022-09-13 15:22:32 -0700

    all is well

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints a-z ten times over
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
