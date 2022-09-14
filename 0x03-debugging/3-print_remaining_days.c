315c78db9ef02165347f2b96d26e98174d567df4
olibrahim <olayodeibrahim123@gmail.com>
2022-09-13 17:12:52 -0700

    all is well

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
  * @day: day of month
  * @year: year
  * Return: void
- */
+*/
 
 void print_remaining_days(int month, int day, int year)
 {
@@ -29,7 +29,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day)
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
